26ac8b8ec6a9a8ae47d6e72bef3e5b885a6ca159
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-08-03 01:35:15 +0000

    finish code

--- src/models.py
+++ src/models.py
@@ -1,85 +1,65 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+from datetime import datetime
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'User'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    UserName = Column(String(100), unique=True, nullable=False)
-    FirstName = Column(String(50), unique=True, nullable=False)
-    LastName = Column(String(50),)
-    Email = Column(String(50), unique=True, nullable=False)
+    ignore = Column(Integer,primary_key=True)
+    user_from_id = Column(Integer,ForeignKey('user.id'))
+    user_to_id = Column(Integer,ForeignKey('user.id'))
 
-class Favorite_Vehicle(Base):
-    __tablename__ = 'Favorite_Vehicle'
-    Userid = Column(String(100), ForeignKey('User.id'), nullable=False, primary_key=True)
-    Vehicleid = Column(Integer, ForeignKey('Vehicle.id') )
 
-class Favorite_People(Base):
-    __tablename__ = 'Favorite_People'
-    Userid = Column(String(100), ForeignKey('User.id'), nullable=False, primary_key=True)
-    Peopleid = Column(Integer, ForeignKey('People.id'), unique=True, nullable=False)
-
-class Favorite_Planets(Base):
-    __tablename__ = 'Favorite_Planets'
-    Userid = Column(String(100), ForeignKey('User.id'), nullable=False, primary_key=True)
-    Planetsid = Column(Integer, ForeignKey('Planets.id'))
-    
-class Vehicle(Base):
-    __tablename__ = 'Vehicle'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    Name = Column(String(250), nullable=False)
-    Model = Column(String(250))
-    Manufacturer = Column(String(250), nullable=False)
-    CostInCredits = Column(Integer)
-    Length = Column(Integer)
-    max_atmosphering_speed = Column(Integer, unique=True, nullable=False)
-    Crew = Column(Integer, unique=True, nullable=False)
-    Passengers = Column(Integer, unique=True, nullable=False)
-    cargo_capacity = Column(Integer, unique=True, nullable=False)
-    Consumables = Column(String(100), unique=True, nullable=False)
-    vehicle_class = Column(String(100), unique=True, nullable=False)
-    Pilots = Column(String(100), unique=True, nullable=False)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    biography = Column(String(250))
+    email = Column(String(250), ForeignKey('person.id'))
+    followers = relationship('Follower')
+    comment = relationship('Comment')
+    post = relationship('Post')
 
-class People(Base):
-    __tablename__ = 'People'
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    Name = Column(String(250), nullable=False)
-    Heigth = Column(Integer) 
-    Mass = Column(Integer)
-    HairColor = Column(String(250), nullable=False)
-    SkinColor = Column(String(250), nullable=False)
-    eye_color = Column(String(250), nullable=False)
-    Birth_Year = Column(String(250), nullable=False)
-    gender = Column(String(250), nullable=False)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
-class Planets(Base):
-    __tablename__ = 'Planets'
-    ID = Column(Integer, primary_key=True)
-    Name = Column(String(250), nullable=False)
-    rotation_period = Column(Integer)
-    orbital_period = Column(Integer)
-    diamneter = Column(Integer)
-    Climate = Column(String(250), nullable=False)
-    Gravity = Column(String(250), nullable=False)
-    terrain = Column(String(250), nullable=False)
-    surface_water = Column(String(250), nullable=False)
-    population = Column(String(250), nullable=False) 
-    Residents = Column(String(250), nullable=False)
-    population = Column(String(250), nullable=False)
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id')) 
+    title = Column(String(250))
+    description = Column(String(250))
+    upload_date = Column(DateTime)
+    like_number = Column(Integer)
+    comment = relationship('Comment')
+    post = relationship('Media')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    src_url = Column(String(250))
+    post_id = Column(Integer,ForeignKey('post.id')) 
+
 
     def to_dict(self):
         return {}
-
-## Draw from SQLAlchemy base
+        
 render_er(Base, 'diagram.png')
+
